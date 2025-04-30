d499115e4862a731dadf0e8baddb227a0191c31c
Tomasz <tomasz.iwanicki@onet.pl>
2025-04-30 18:28:48 +0200

    Dodanie logiki oceny układu kart (hand_rank)

--- poker.py
+++ poker.py
@@ -1,5 +1,23 @@
 import random
 
+def histogram(lst):
+    hist = {}
+    for item in lst:
+        hist[item] = hist.get(item, 0) + 1
+    return hist
+
+def is_rank_sequence(hand):
+    rank_order = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    ranks = [card[0] for card in hand]
+
+    if 'A' and '2' and '3' and '4' and '5' in ranks:
+        ranks = ['1' if rank == 'A' else rank for rank in ranks]
+
+    rank_indices = [rank_order.index(rank) for rank in ranks]
+    rank_indices.sort()
+
+    return rank_indices == list(range(rank_indices[0], rank_indices[0] + 5))
+
 class Card:
     unicode_dict = {'s': '\u2660', 'h': '\u2665', 'd': '\u2666', 'c': '\u2663'}
 
@@ -57,6 +75,52 @@ class Player:
 
     def get_name(self):
         return self.__name_
+
+
+def hand_rank(hand):
+    hand_rank_list = [card[0] for card in hand]
+    hand_color_list = [card[1] for card in hand]
+
+    hand_rank_histogram = histogram(hand_rank_list)
+    hand_color_histogram = histogram(hand_color_list)
+
+    is_hand_rank_sequence = is_rank_sequence(hand)
+
+    hand_strength = 0
+
+    # Sprawdzamy poker królewski: 5 kart w tym samym kolorze, po kolei, najwyższa to as
+    if (5 in hand_color_histogram.values()) and ('A' in hand_rank_list) and is_hand_rank_sequence:
+        hand_strength = 10
+    # Sprawdzamy poker: 5 kart w tym samym kolorze, po kolei
+    elif (5 in hand_color_histogram.values()) and is_hand_rank_sequence:
+        hand_strength = 9
+    # Sprawdzamy karete: 4 karty tej samej rangi
+    elif 4 in hand_rank_histogram.values():
+        hand_strength = 8
+    # Sprawdzamy full house: 3 karty tej samej rangi i 2 karty tej samej rangi
+    elif sorted(hand_rank_histogram.values()) == [2, 3]:
+        hand_strength = 7
+    # Sprawdzamy kolor: 5 kart w tym samym kolorze
+    elif 5 in hand_color_histogram.values():
+        hand_strength = 6
+    # Sprawdzamy strit: 5 kart po kolei
+    elif is_hand_rank_sequence:
+        hand_strength = 5
+    # Sprawdzamy trojkę: 3 karty tej samej rangi
+    elif 3 in hand_rank_histogram.values():
+        hand_strength = 4
+    # Sprawdzamy dwie pary: 2 pary
+    elif list(hand_rank_histogram.values()).count(2) == 2:
+        hand_strength = 3
+    # Sprawdzamy jedną parę: 1 para
+    elif 2 in hand_rank_histogram.values():
+        hand_strength = 2
+    # Wysoka karta: brak par, trojek, czwórek itd.
+    else:
+        hand_strength = 1
+
+    return hand_strength
+
 
 player1 = Player(1000, "Player1")
 player2 = Player(1000, "Player2")
